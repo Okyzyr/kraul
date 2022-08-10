87ae1c8778c07c0913f54ab305de9d1f2dddf2dc
kcukierski@gmail.com <Liteon321?>
2022-08-10 13:09:25 +0200

    konfiguracja slenium

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from selenium import webdriver
 PATH = "/Users/kuba/Documents/chromedriver"
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://www.google.com")
+driver.get("https://mediamarkt.pl")
